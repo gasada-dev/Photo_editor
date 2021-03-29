f471bacbf0572eb7a7abfa6887e76f295c749084
gasada <gasada@protonmail.com>
2021-03-30 02:42:46 +0300

    refactor: small refactor in index.js and del blank lines in index.html

--- src/js/index.js
+++ src/js/index.js
@@ -5,5 +5,9 @@ function handleUpdate() {
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+const start = () => {
+  inputs.forEach(input => input.addEventListener('change', handleUpdate));
+  inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+};
+
+start();
